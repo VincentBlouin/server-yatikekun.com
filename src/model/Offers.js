48421ee59f7a6b077ef404e90fb71aa7272e2b27
Vincent Blouin <vincent.blouin@gmail.com>
2020-11-29 19:24:54 -0500

    offers

--- src/model/Offers.js
+++ src/model/Offers.js
@@ -2,6 +2,10 @@ module.exports = (sequelize, DataTypes) => {
     const Offers = sequelize.define('Offers', {
         title_fr: DataTypes.STRING,
         title_en: DataTypes.STRING,
+        experience_fr: DataTypes.STRING,
+        experience_en: DataTypes.STRING,
+        additionalFees_fr: DataTypes.STRING,
+        additionalFees_en: DataTypes.STRING,
         image: DataTypes.STRING,
         customImage: DataTypes.JSONB,
         isAvailable: DataTypes.BOOLEAN
