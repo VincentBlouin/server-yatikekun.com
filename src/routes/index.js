d7f870f81ba8da5cc1dfdec0de97664095aecf17
Vincent Blouin <vincent.blouin@gmail.com>
2021-06-02 16:36:53 -0400

    facebook login

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,42 +23,10 @@ const isAdmin = require('../policy/isAdmin')
 //   AuthenticationController.register
 // )
 
-const passport = require('passport')
-
-FacebookStrategy = require('passport-facebook').Strategy;
-if(config.getConfig().appId !== ""){
-    passport.use(new FacebookStrategy({
-            clientID: config.getConfig().appId,
-            clientSecret: config.getConfig().fb,
-            callbackURL: "https://partageheure.com/api/auth/facebook/callback",
-            profileFields: ["email"]
-        },
-        AuthenticationController.facebookLogin
-    ));
-}
-
 router.post(
     '/login',
     AuthenticationController.login
 )
-
-router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['publish_to_groups', 'email'] }));
-
-router.get('/auth/facebook/success', (req, res) => {
-    res.send("Success");
-});
-
-router.get('/auth/facebook/fail', (req, res) => {
-    res.send("Fail");
-});
-
-router.get(
-    "/auth/facebook/callback",
-    passport.authenticate("facebook", {
-        successRedirect: "/api/auth/facebook/success",
-        failureRedirect: "/api/auth/facebook/fail"
-    })
-);
 
 router.post(
     '/reset-password',
